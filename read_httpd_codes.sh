976faf034ce4405a6d451ec0a5c27c06c2067cc3
Matthew <mdavidson1965@gmail.com>
2018-11-25 15:15:59 -0500

    Remove unused lines of code

--- read_httpd_codes.sh
+++ read_httpd_codes.sh
@@ -32,7 +32,6 @@ pull_logs(){
 
 extract_codes(){
 	#Extract response codes and count
-	#awk '{if ($9 ~ /^[0-9][0-9][0-9]$/) print $9}' ${ACCESS} | sort -n | uniq -c > ${HTTPDCODES}
 	awk '{print $9}' ${ACCESS} | sort | uniq -c | sort -rn | awk '{print $2 "=" $1}' > ${HTTPDCODES}
 }
 
@@ -41,7 +40,6 @@ process_codes(){
 	#This awk line will process all lines in ${HTTPDCODES} and put them inline so
 	# inputs.exec / telegraf can send them to influxdb.
 	# The added i is to let influxdb know these are integers. 
-	#awk '{a=(NR>1?a"i"",":"")$1} END {print "myhttpd_response_code"" " a"i"}' /tmp/httpd_codes >> /home/matthew/my_code_results
 	awk '{a=(NR>1?a"i"",":"")$1} END {print "myhttpd_response_code"" " a"i"}' /tmp/httpd_codes
 }
 
